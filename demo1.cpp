a59a33d28db85a539dab6db8966a13ddd854cc11
liu-tianming <118498157+liu-tianming@users.noreply.github.com>
2023-12-24 17:11:27 +0800

    Initial commit

---
+++ demo1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+#include<vector>
+
+int main() {
+    double l = 10;//设橡皮筋原本为10米
+    //vector<double> v;//用一个动态数组记录第n天的时候蚂蚁没走长度
+
+    double cover = 0;//蚂蚁走过的橡皮筋长度
+    int n = 0;
+
+    while (l>0)
+    {
+        n++;
+        cover += 1;
+        l = l - 1;
+        cover = cover + cover / n;
+        l = l + l / n;
+    }
+    cout << "n=" << n << endl;
+
+
+    
+
+}
